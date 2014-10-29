3c145c0576ec8da728703af5d9744c78e9eeb612
Angela <banuelosangela56@gmail.com>
2014-10-29 11:09:43 -0700

    Added comments

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,19 +1,25 @@
 <?php
+//this looks for database.php
 require_once(__DIR__ . "/../model/database.php");
-
+//this looks for the indicated varaibles
 $connection = new mysqli($host, $username, $password);
 
-
+//thsi checks if there is an error i our connection
 if($connection->connect_error){
 die("Error: " . $connection->connect_error);
 }
+//checks if select exists
 $exists = $connection->select_db($database);
+//checks if databse doesnt exist 
 if(!$exists){
+	//if not, then you create one.
 	$query = $connection->query("CREATE DATABASE $database");
 	if($query){
+		//jif it created it prints out the sentance
 		echo "Successfully created database: " . $database;
 	}
 }
+//database is already there
 else{
 	echo "Database already exists";
 }
@@ -23,4 +29,11 @@ $query = $connection->query("CREATE TABLE posts ("
 	. "post text NOT NULL,"
 	. "PRIMARY KEY (id))");
 
+if($query) {
+		echo "<p>Successfully created table: posts</p>";
+	}
+else{
+	echo "<p>$connection->error</p>";
+}
+//closes connection
 $connection->close();
--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 </head>
 <body>
 	<?php
-	//
+	//this is where youll have html code and get info from the model
+	//the DIR concatinates 
 	require_once(__DIR__ . "/controller/create-db.php");
 	?>
 </body>
