b270b8eefd2c287ab937f89a18edf6263ae27955
Angela <banuelosangela56@gmail.com>
2014-11-03 12:07:49 -0800

    Commented in the index(header and footer)

--- index.php
+++ index.php
@@ -1,8 +1,12 @@
 	<?php
+	//uisng the header file
+	//it stores the header html code in the header.php file 
 	require_once(__DIR__ . "/view/header.php");
 	//this is where youll have html code and get info from the model
 	//the DIR concatinates 
 	require_once(__DIR__ . "/controller/create-db.php");
+	//using the footer file
+	//it stores the footer html code in the footer.php file
 	require_once(__DIR__ . "/view/footer.php");
 	?>
 
