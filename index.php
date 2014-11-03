2907caaa71396faf3bcd650f023a7fd2913563aa
Angela <banuelosangela56@gmail.com>
2014-11-03 12:00:38 -0800

    Created header and footer

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
 	<?php
+	require_once(__DIR__ . "/view/header.php");
 	//this is where youll have html code and get info from the model
 	//the DIR concatinates 
 	require_once(__DIR__ . "/controller/create-db.php");
+	require_once(__DIR__ . "/view/footer.php");
 	?>
-</body>
-</html>
+
