97b99726fe5446fffe31e7eea07f4017c0bc9fac
Angela <banuelosangela56@gmail.com>
2014-11-04 11:12:07 -0800

    I created a navigation file and a post file

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 	//uisng the header file
 	//it stores the header html code in the header.php file 
 	require_once(__DIR__ . "/view/header.php");
+	//using the navigation file 
+	//it stores the info in navigation.
+	require_once(__DIR__ . "/view/navigation.php");
 	//this is where youll have html code and get info from the model
 	//the DIR concatinates 
 	require_once(__DIR__ . "/controller/create-db.php");
---
+++ post.php
@@ -0,0 +1,4 @@
+<?php
+//its call header.php, form.php, and footer.php
+require_once(__DIR__ . "/view/header.php");
+require_once(__DIR__ . "/view/footer.php");
