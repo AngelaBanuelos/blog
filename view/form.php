0a36a00fb2de88876ddd015b5a99140cf37c21bb
Angela <banuelosangela56@gmail.com>
2014-11-05 11:09:30 -0800

    Added a title and post input box

---
+++ view/form.php
@@ -0,0 +1,16 @@
+<h1>Create Blog Post</h1>
+
+<form>
+	<div>
+		<!-- it labels the title -->
+		<label for="title">Title: </label>
+		<!-- makes a text box where you can insert only one line text -->
+		<input type="text" name="title" />
+	</div>
+	<div>
+		<!-- it labels the Post -->
+		<label for="post">Post: </label>
+		<!-- makes a text box where you can insert multiple lines of text -->
+		<textarea name=""post></textarea>
+	</div>
+</form>
--- view/header.php
+++ view/header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="main2.css">
 	<title></title>
 </head>
 <body>
