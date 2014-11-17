a62ee2f169f7c530cc806cbc0f445450357e72d3
Angela <banuelosangela56@gmail.com>
2014-11-17 12:04:50 -0800

    videos 27 and 18; added comments to database

--- model/database.php
+++ model/database.php
@@ -11,4 +11,29 @@ class Database {
 	private $password;
 	//private makes the variables unavailable to other classes. 
 	private $database;
+	//local variables
+	//this is the constructor of the class
+	//this construct function is needed in order to make the objects 
+	public function __construct($host, $username, $password, $database) {
+		//setting our global variables to our local variables 
+		$this->host = $host;
+		$this->username = $username;
+		$this->password = $password;
+		$this->database = $database;
+	}
+	//this function opens the connection
+	public function openConnection(){
+
+	}
+	//this function closes the connection
+	public function closeConnection(){
+
+
+	}
+	//every time we call the query we have to write a string 
+	//in the end it wil be passed as a variable 
+	public function query($string){
+
+
+	}
 }
