503d0056cd19c420d4599c2c3285342ed03c8ce1
Angela <banuelosangela56@gmail.com>
2014-12-02 11:05:41 -0800

    Coded for video 37; created a new register-form.php file and added inputs and labels within the file; commented.

---
+++ view/register-form.php
@@ -0,0 +1,27 @@
+<form>
+	<div>
+		<!-- it labels the email	-->
+		<label for = "email">Email:</label>
+		<!-- creates a text box where you can input a single line with the text being visible-->
+		<input type="text" name="email" />
+	</div>
+
+	<div>
+		<!-- it labels the username	-->
+		<label for = "user">Username:</label>
+		<!-- creates a text box where you can input a single line with the text being visible -->
+		<input type="text" name="username" />
+	</div>	
+
+	<div>
+		<!-- it labels the password	-->
+		<label for = "password">Password:</label>
+		<!-- creates a text box where you can input a single line with the text invisble-->
+		<input type="password" name="password" />
+	</div>
+	<div>
+		<!-- creates a button to submit	-->
+		<button type="submit">Submit</button>
+	</div>
+</form>
+
