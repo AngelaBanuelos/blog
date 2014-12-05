680c3f0be3d8f5ffcdcef93c4988b3b3723570ef
Angela <banuelosangela56@gmail.com>
2014-12-05 11:06:12 -0800

    Coded videos 38,39, and 40;

--- controller/create-post.php
+++ controller/create-post.php
@@ -5,6 +5,11 @@ require_once(__DIR__ . "/../model/config.php");
 $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
 //the post means to get the data and filter it
 $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+
+$date = new DateTime('today');
+$time = new DateTime('America/Los_Angeles');
+
+
 //run query to insert things into table
 //sql command starts off with an active/verb
 $query = $_SESSION["connection"]->query("INSERT INTO posts SET title = '$title', post = '$post'");
@@ -13,7 +18,7 @@ $query = $_SESSION["connection"]->query("INSERT INTO posts SET title = '$title',
 if($query){
 		// if successful outputs a title 
 		echo "<p>Successfully inserted post: $title and $post</p>";
-
+		echo  " Posted on: " . $date->format("M/D/Y") . "at" . $time->format("g:i");
 }
 else{
 	// if not successful dusplay and error
