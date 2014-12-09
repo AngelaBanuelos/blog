412559c088f31780f4e07fbc2aeaa50667228d97
Angela <banuelosangela56@gmail.com>
2014-12-09 10:43:14 -0800

    added html code to create-post

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="css/main.css">
+	<title></title>
+</head>
+<body>
+
+
 <?php
 //require database file
 require_once(__DIR__ . "/../model/config.php");
@@ -20,11 +29,13 @@ if($query){
 		// if successful outputs a title 
 		echo "<p>Successfully inserted post: $title and $post</p>";
 		//echos out the date and time
-		echo  " Posted on: " . $date->format("M/D/Y") . "at" . $time->format("g:i");
+		echo  " Posted on: " . $date->format("m/d/y") . "at" . $time->format("g:i");
 }
 else{
 	// if not successful dusplay and error
 	echo "<p>" . $_SESSION["connection"]->error . "</p>";
 }
 
-
+?>
+</body>
+</html>
