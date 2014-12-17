93053d6a0ed9b320a879f99fa3dd696a6792ab92
Angela <banuelosangela56@gmail.com>
2014-12-16 22:58:53 -0800

    Changed carousel pictures

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
                 <!-- Wrapper for slides -->
                 <div class="carousel-inner" role="listbox">
                     <div class="item active">
-                        <img src="J1.jpg" alt="...">
+                        <img src="J2.jpg" alt="...">
                         <div class="carousel-caption">
                             ...
                         </div>
@@ -50,19 +50,19 @@
                         </div>
                     </div>
                     <div class="item">
-                        <img src="J4.jpg" alt="...">
+                        <img src="J1.jpg" alt="...">
+                        <div class="carousel-caption">
+                            ...
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img src="J6.jpg" alt="...">
                         <div class="carousel-caption">
                             ...
                         </div>
                     </div>
                     <div class="item">
                         <img src="J3.jpg" alt="...">
-                        <div class="carousel-caption">
-                            ...
-                        </div>
-                    </div>
-                    <div class="item">
-                        <img src="J2.jpg" alt="...">
                         <div class="carousel-caption">
                             ...
                         </div>
