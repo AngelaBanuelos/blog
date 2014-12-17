38148cf966c971cf97acfa01dfc9bc3fef33038d
Angela <banuelosangela56@gmail.com>
2014-12-16 21:48:29 -0800

    Commented create-user.php

--- controller/create-user.php
+++ controller/create-user.php
@@ -1,16 +1,19 @@
 <?php
+//requires the config file so that this file can use the info in the config page
 require_once(__DIR__ . "/../model/config.php");
 require_once(__DIR__ . "/../controller/login-verify.php");
-
+//this variable stores the emial of a user to the databse 
 $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+//this variable stores the username of a user to the database
 $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+//this variable stores the passwrod of a user to the database
 $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
 
-
+//this variable creates a hash so that the passwprds are unique and protected
 $salt = "$5$" . "rounds=5000$" . uniqid(mt_rand(), true) . "$";
 
 $hashedPassword = crypt($password, $salt);
-
+//creates a query so that i can storethe encrypted passwords in database using sessions
 $query = $_SESSION["connection"]->query("INSERT INTO users SET "
 	. "email = '$email', "
 	. "username = '$username', "
