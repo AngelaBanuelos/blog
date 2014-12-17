f06eb9627b40ca97a7992d4c95d9673ffaf8cc15
Angela <banuelosangela56@gmail.com>
2014-12-16 22:02:34 -0800

    commented login-verify.php

--- controller/login-verify.php
+++ controller/login-verify.php
@@ -1,6 +1,9 @@
 <?php
+//we require config file so we can use the info we need
 	require_once(__DIR__ . "/../model/config.php");
-	
+	//checks if their is a session variable and if its true
+	//if its not true, the user isnt logged in 
+	//the if statements is used to check if session authenticated hasnt been set
 	//to verify if user has logged in
 	function authenticateUser() {
 		if(!isset($_SESSION["authenticated"])) {
@@ -8,6 +11,7 @@
 		}
 		
 		else{
+			//checking session variable has info 
 			if($_SESSION["authenticated"] != true){
 				return false;
 			}
